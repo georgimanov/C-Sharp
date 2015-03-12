9d3270f0b70a4142003241023c2e7477e5957aeb
Manov <georgimanov@gmail.com>
2015-03-12 21:08:59 +0200

    [HW][OOP] Defining classes part 2 8 Matrix; 9 Matrix indexer;

---
+++ Lectures/15 DefiningClassesPartTwo/Maxtrix/MatrixClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxtrix
+{
+    class MatrixClient
+    {
+        static void Main(string[] args)
+        {
+            var rows = 4;
+            var cols = 4;
+            var matrix = new Matrix<int>(rows, cols);
+
+            var counter = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix.SetElement(row, col, counter++);
+                }
+            }
+
+            var getRow = 1;
+            var getCol = 1;
+            matrix.PrintMatrix();
+            Console.WriteLine("matrix[{0}, {1}] = {2}", getRow, getCol, matrix.GetElement(1, 1));
+        }
+    }
+}
---
+++ Lectures/15 DefiningClassesPartTwo/Maxtrix/Maxtrix.cs
@@ -0,0 +1,67 @@
+﻿namespace Maxtrix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Data.Common;
+
+    public class Matrix<T>
+    {
+        private const int InitialSizeRows = 16;
+        private const int InitialSizeCols = 16;
+
+        private T[,] matrix;
+
+        public Matrix()
+            : this(InitialSizeRows, InitialSizeCols)
+        {
+            this.matrix = new T[InitialSizeRows, InitialSizeCols];
+        }
+
+        public Matrix(int initialSizeRows, int initialSizeCols)
+        {
+            if (initialSizeRows < 2 || initialSizeCols < 2)
+            {
+                throw new IndexOutOfRangeException("Initial size (rows/cols) must be bigger than 2");
+            }
+
+            this.matrix = new T[initialSizeRows, initialSizeCols];
+        }
+
+        public T GetElement(int row, int col)
+        {
+            if ( ! ((this.matrix.GetLength(0) > row && row >= 0) &&
+                 (this.matrix.GetLength(1) > col && col >= 0)))
+            {
+                throw new IndexOutOfRangeException("rows/cols must be inside matrix");
+            }
+
+            return this.matrix[row, col];
+        }
+
+        public void SetElement(int row, int col, T value)
+        {
+            if (!((this.matrix.GetLength(0) > row && row >= 0) &&
+                 (this.matrix.GetLength(1) > col && col >= 0)))
+            {
+                throw new IndexOutOfRangeException("rows/cols must be inside matrix");
+            }
+
+            this.matrix[row, col] = value;
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < this.matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0}".PadRight(8), this.matrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
