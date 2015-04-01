81bdb73c3e6515b98381962dfdd6af14b21cf404
Manov <georgimanov@gmail.com>
2015-04-01 19:48:07 +0300

    Custom ecpetion

---
+++ CustomException/CustomException/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new TestException();
+            }
+            catch (TestException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ CustomException/CustomException/TestException.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomException
+{
+    using System;
+
+    public class TestException : ApplicationException
+    {
+        public override string Message
+        {
+            get { return "Something bad happened!"; }
+        }
+
+        public TestException()
+        {
+        }
+    }
+}
