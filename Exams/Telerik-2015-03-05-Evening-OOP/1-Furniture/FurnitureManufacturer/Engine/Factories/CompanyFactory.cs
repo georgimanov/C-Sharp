886a2c959ba4e82287d656755a400bf4fddcc3b1
georgi <georgimanov@gmail.com>
2015-04-03 17:17:03 +0300

    [Telerik][OOP][Exam] - 05 March 2014 - Evening

---
+++ Exams/Telerik-2015-03-05-Evening-OOP/1-Furniture/FurnitureManufacturer/Engine/Factories/CompanyFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FurnitureManufacturer.Engine.Factories
+{
+    using Interfaces;
+    using Interfaces.Engine;
+    using Models;
+
+    public class CompanyFactory : ICompanyFactory
+    {
+        
+        public ICompany CreateCompany(string name, string registrationNumber)
+        {
+            return new Company(name, registrationNumber);
+        }
+    }
+}
