e7fd8af5c15ad060d9150b4780b2606cd7bec6c2
georgi <georgimanov@gmail.com>
2015-04-04 16:55:38 +0300

    [Telerik][OOP][2013-12-12] 1.WarMachines

---
+++ Exams/Telerik-2013-12-12/1. War Machines_Условие на задачата/WarMachines-Skeleton/WarMachines/Machines/Fighter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WarMachines.Interfaces;
+
+namespace WarMachines.Machines
+{
+    public class Fighter : Machine, IFighter
+    {
+        private const double initialHealthPoints = 200;
+        public Fighter(string name, double attackPoints, double defensePoints, bool stealthMode)
+            : base (name, attackPoints, defensePoints, Fighter.initialHealthPoints)
+        {
+            this.StealthMode = stealthMode;
+        }
+        
+        public bool StealthMode { get ; private set; }
+        public void ToggleStealthMode()
+        {
+            this.StealthMode = !this.StealthMode;
+        }
+        public override string ToString()
+        {
+            return base.ToString() +
+                Environment.NewLine +
+                string.Format(" *Stealth: {0}", this.StealthMode ? "ON" : "OFF");
+        }
+    }
+}
