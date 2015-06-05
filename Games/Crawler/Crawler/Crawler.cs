1be8fb1a597c1d192baa473e68acdc45bcecf4c9
georgimanov <georgimanov@gmail.com>
2015-06-05 19:33:09 +0300

    Spiderman

---
+++ Games/Crawler/Crawler/Crawler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Crawler
+{
+
+    using System.Net;
+
+    public class Crawler
+    {
+        public StringBuilder GetHtml(string url)
+        {
+            string urlAddress = url;
+            var data = new StringBuilder();
+
+            var request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = new StringBuilder(readStream.ReadToEnd());
+
+                response.Close();
+                readStream.Close();
+            }
+
+            return data;
+        }
+
+
+        public void SaveHtmlContent(StringBuilder content, string fileName)
+        {
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            
+            // Write the stream contents to a new file named "AllTxtFiles.txt".
+            using (var outfile = new StreamWriter(mydocpath + @"\"+ fileName + ".txt"))
+            {
+                outfile.Write(content.ToString());
+            }
+        }
+
+        public bool CheckIfUserExistsInForumTheme(string username, StringBuilder file)
+        {
+            return file.ToString().Contains(username);
+        }
+
+    }
+}
+ 
---
+++ Games/Crawler/Crawler/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const string baseUrl = "https://softuni.bg/users/profile/show/";
+            //const string baseUrl = "https://softuni.bg/forum?page=0&pageSize=5000";
+            //get content from forum: https://softuni.bg/forum/4400/0/100
+
+            const string fileName = "content";
+            const string user = "RoYaL";
+            const string forumBaseUrl = "https://softuni.bg/forum/";
+
+            var list = new List<int>();
+
+
+            for (int i = 4; i < 4500; i++)
+            {
+                var spider = new Crawler();
+                var htmlContent = spider.GetHtml(forumBaseUrl + i + "/0/100");
+                Console.Write("Forum [" + i +"]");
+                if (htmlContent.ToString().Contains(user))
+                {
+                    list.Add(i);
+                    Console.Write("[1]");
+                }
+                else
+                {
+                    Console.Write("[0]");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Completed");
+            Console.WriteLine(string.Join(", ", list));
+            Console.ReadLine();
+            //try
+            //{
+            //    var spider = new Crawler();
+
+            //    var htmlContent = spider.GetHtml(forumBaseUrl);
+            //    spider.SaveHtmlContent(htmlContent, fileName);
+
+            //}
+            //catch (Exception e)
+            //{
+            //    throw new Exception("There's an error");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Process comepleted!");
+            //    Console.ReadLine();
+            //}
+        }
+    }
+}
